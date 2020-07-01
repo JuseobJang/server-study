72326f3d6264942c783790e4a5b3fdf230b09ced
JuseobJang <wntjq68@gmail.com>
2020-07-01 17:55:39 +0900

    style: Modify main.js style
    - function() change to '=>'.
    - It add to "404 Not found" for not existed file.

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-console.log(fs);
+// console.log(fs);
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  // console.log(_url);
-
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      var description = "Hello, Node.js";
-      // fs.readFile(`./data/${title}`,'utf-8',function(err,description){
-
-      var title = "welcome";
+      var title = "Welcome";
       var description = "Hello, Node.js";
       var template = `
           <!doctype html>
@@ -39,30 +33,31 @@ var app = http.createServer(function (request, response) {
         `;
       response.writeHead(200);
       response.end(template);
-
-      // });
     } else {
-      fs.readFile(`./data/${title}`, "utf-8", function (err, description) {
+      fs.readFile(`./data/${queryData.id}`, "utf-8", (err, description) => {
+        if (err) {
+          throw err;
+        }
         var title = queryData.id;
         var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <U l>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </U>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-        `;
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <U l>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </U>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+      `;
         response.writeHead(200);
         response.end(template);
       });
