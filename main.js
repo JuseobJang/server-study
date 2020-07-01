6d58c491e1e06b8ee1d5a3620595602ed0fc7be7
JuseobJang <wntjq68@gmail.com>
2020-07-01 20:18:07 +0900

    feat: Add new function that create new file by receiving data.
    This code create new file by reciving new data that send by html form ,method is post.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-// console.log(fs);
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -14,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href = "/create">create</a>
     ${body} 
   </body>
   </html>
@@ -64,7 +65,48 @@ var app = http.createServer(function (request, response) {
         });
       });
     }
-  } else {
+  }
+  else if (pathname === "/create") {
+    fs.readdir("./data", (err, filelist) => {
+      if (err) {
+        throw err;
+      }
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(title, list, `
+        <form action="http://localhost:3000/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(template);
+    });
+  }
+  else if (pathname === "/create_process") {
+    var body = ``;
+    request.on('data', (data) => {
+      body += data;
+    })
+    request.on('end', () => {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf-8', (err) => {
+        if (err) throw err;
+        response.writeHead(302, { Location: `/?id=${title}` }); // 302 redirection
+        response.end();
+
+      })
+    })
+
+  }
+  else {
     response.writeHead(404);
     response.end("Not found");
   }
