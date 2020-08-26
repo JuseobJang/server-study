2effe11e2fe054eacd9f2a32a13fb3a41f62958c
JuseobJang <wntjq68@gmail.com>
2020-08-26 19:00:41 +0900

    docs: Add remark to main.js
    It is my review.

--- main.js
+++ main.js
@@ -39,45 +39,45 @@ function templateList(filelist) {
 }
 
 var app = http.createServer(function (request, response) { // Create Server using request & response
-  var _url = request.url; // 요청된 request를 _url에 저장
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
+  var _url = request.url; // 요청된 request url 을 저장
+  var queryData = url.parse(_url, true).query; //url을 parsing 해서 queryString 저장
+  var pathname = url.parse(_url, true).pathname; //url을 parsing 해서 pathname 저장
 
-  if (pathname === "/") {
-    if (queryData.id === undefined) {
-      fs.readdir("./data", (err, filelist) => {
+  if (pathname === "/") { // root directory
+    if (queryData.id === undefined) { //id 미정의 
+      fs.readdir("./data", (err, filelist) => { //read directory
         if (err) {
           throw err;
         }
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a>`);
-        response.writeHead(200);
-        response.end(template);
+        var list = templateList(filelist); // directory 내의 파일을 list화
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a>`); // html 생성
+        response.writeHead(200); //head 생성
+        response.end(template); // html response
       });
-    } else {
+    } else { // id 가 정의 되어 있는 경우
       fs.readdir("./data", (err, filelist) => {
-        fs.readFile(`./data/${queryData.id}`, "utf-8", (err, description) => {
+        fs.readFile(`./data/${queryData.id}`, "utf-8", (err, description) => { //디렉토리 안의 id 와 이름이 같은 파일의 내용을 description으로 넣음
           if (err) {
             throw err;
           }
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a> 
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a>  
           <a href="/update?id=${title}">update</a>
           <form action="delete_process" method = "post">
             <input type = "hidden" name = "id" value="${title}">
             <input type = "submit" value="delete">
           </form>
-          `);
+          `); //descrption 을 내용으로 추가하고 control 인자에 update와 delete가 추가
           response.writeHead(200);
           response.end(template);
         });
       });
     }
   }
-  else if (pathname === "/create") {
+  else if (pathname === "/create") { // 파일 생성 HTM
     fs.readdir("./data", (err, filelist) => {
       if (err) {
         throw err;
@@ -94,7 +94,7 @@ var app = http.createServer(function (request, response) { // Create Server usin
             <input type="submit">
           </p>
         </form>
-      `, ``);
+      `, ``); //form 형식으로 /create_process 로 보냄
       response.writeHead(200);
       response.end(template);
     });
@@ -105,19 +105,19 @@ var app = http.createServer(function (request, response) { // Create Server usin
       body += data;
     })
     request.on('end', () => {
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf-8', (err) => {
+      var post = qs.parse(body); //data 내용 을 qs 로 parsing
+      var title = post.title; // 제목 저장
+      var description = post.description; // 내용 저장
+      fs.writeFile(`data/${title}`, description, 'utf-8', (err) => { // 제목을 파일 명으로 하여 파일 생성
         if (err) throw err;
-        response.writeHead(302, { Location: `/?id=${title}` }); // 302 redirection
+        response.writeHead(302, { Location: `/?id=${title}` }); // 302 redirection 
         response.end();
 
       })
     })
 
   }
-  else if (pathname === '/update') {
+  else if (pathname === '/update') { // update HTML form 생성
     fs.readdir('./data', (err, filelist) => {
       fs.readFile(`data/${queryData.id}`, 'utf8', (err, description) => {
         var title = queryData.id;
@@ -153,8 +153,8 @@ var app = http.createServer(function (request, response) { // Create Server usin
       var title = post.title;
       var description = post.description;
 
-      fs.rename(`data/${id}`, `data/${title}`, (err,) => {
-        fs.writeFile(`data/${title}`, description, 'utf-8', (err) => {
+      fs.rename(`data/${id}`, `data/${title}`, (err,) => { // 이름 변경
+        fs.writeFile(`data/${title}`, description, 'utf-8', (err) => { // 파일 쓰기 
           if (err) throw err;
           response.writeHead(302, { Location: `/?id=${title}` }); // 302 redirection
           response.end();
@@ -163,7 +163,7 @@ var app = http.createServer(function (request, response) { // Create Server usin
       })
     })
   }
-  else if (pathname === '/delete_process') {
+  else if (pathname === '/delete_process') { // 
     var body = ``;
     request.on('data', (data) => {
       body += data;
