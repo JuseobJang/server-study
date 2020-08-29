9f02c2383b88416fc69efb0b586858fa1211c368
JuseobJang <wntjq68@gmail.com>
2020-08-29 18:17:08 +0900

    refactor: Use middle-ware(compression)
    It uses the'compression' middle ware to send and receive data in compressed form. And it defines new middleware and saves the file list in'./data' in request.list for all requests of get method.

--- main.js
+++ main.js
@@ -6,60 +6,59 @@ var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
 
+//middle-ware
 var bodyParser = require('body-parser');
+var compresstion = require('compression');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compresstion());
 
-
+app.get('*', function (request, response, next) {
+  fs.readdir("./data", (err, filelist) => { //read directory
+    request.list = filelist;
+    next();
+  });
+})
 
 const port = 3000
 
 app.get('/', (request, response) => {
-  fs.readdir("./data", (err, filelist) => { //read directory
-    if (err) {
-      throw err;
-    }
-    var title = "Welcome";
-    var description = "Hello, Node.js";
-    var list = template.list(filelist); // directory 내의 파일을 list화
-    var HTML = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a>`); // html 생성
-    response.send(HTML); // html response
-  });
+  var title = "Welcome";
+  var description = "Hello, Node.js";
+  var list = template.list(request.list); // directory 내의 파일을 list화
+  var HTML = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href = "/create">create</a>`); // html 생성
+  response.send(HTML); // html response
 })
 
 app.get('/page/:pageId', (request, response) => {
-  fs.readdir("./data", (err, filelist) => {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`./data/${filteredId}`, "utf-8", (err, description) => { //디렉토리 안의 id 와 이름이 같은 파일의 내용을 description으로 넣음
-      if (err) {
-        throw err;
-      }
-      var title = request.params.pageId;
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`./data/${filteredId}`, "utf-8", (err, description) => { //디렉토리 안의 id 와 이름이 같은 파일의 내용을 description으로 넣음
+    if (err) {
+      throw err;
+    }
+    var title = request.params.pageId;
 
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description);
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description);
 
-      var list = template.list(filelist);
-      var HTML = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, `<a href = "/create">create</a>
+    var list = template.list(request.list);
+    var HTML = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, `<a href = "/create">create</a>
       <a href="/update/${sanitizedTitle}">update</a>
       <form action="/delete_process" method = "post">
         <input type = "hidden" name = "id" value="${sanitizedTitle}">
         <input type = "submit" value="delete">
       </form>
       `); //descrption 을 내용으로 추가하고 control 인자에 update와 delete가 추가
-      response.send(HTML);
-    });
+    response.send(HTML);
   });
 })
 
 app.get('/create', (request, response) => {
-  fs.readdir("./data", (err, filelist) => {
-    if (err) {
-      throw err;
-    }
-    var title = "WEB - create";
-    var list = template.list(filelist);
-    var HTML = template.HTML(title, list, `
+
+  var title = "WEB - create";
+  var list = template.list(request.list);
+  var HTML = template.HTML(title, list, `
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -70,24 +69,10 @@ app.get('/create', (request, response) => {
         </p>
       </form>
     `, ``); //form 형식으로 /create_process 로 보냄
-    response.send(HTML);
-  });
+  response.send(HTML);
 })
 
 app.post('/create_process', (request, response) => {
-  // var body = ``;
-  // request.on('data', (data) => {
-  //   body += data;
-  // })
-  // request.on('end', () => {
-  //   var post = qs.parse(body); //data 내용 을 qs 로 parsing
-  //   var title = post.title; // 제목 저장
-  //   var description = post.description; // 내용 저장
-  //   fs.writeFile(`data/${title}`, description, 'utf-8', (err) => { // 제목을 파일 명으로 하여 파일 생성
-  //     if (err) throw err;
-  //     response.redirect(`/page/${title}`);
-  //   })
-  // })
 
   var post = request.body; // using body-parser 
   var title = post.title;
@@ -99,28 +84,26 @@ app.post('/create_process', (request, response) => {
 })
 
 app.get('/update/:pageId', (request, response) => {
-  fs.readdir('./data', (err, filelist) => {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var HTML = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type ="hidden" name ="id" value ="${title}">
-          <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
-          <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      response.send(HTML);
-    });
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    var title = request.params.pageId;
+    var list = template.list(request.ist);
+    var HTML = template.HTML(title, list,
+      `
+      <form action="/update_process" method="post">
+        <input type ="hidden" name ="id" value ="${title}">
+        <p><input type="text" name="title" placeholder="title" value ="${title}"></p>
+        <p>
+        <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    response.send(HTML);
   });
 })
 app.post('/update_process', (request, response) => {
@@ -149,8 +132,6 @@ app.post('/delete_process', (request, response) => {
   fs.unlink(`data/${filteredId}`, (err) => {
     response.redirect('/');
   })
-
-
 })
 
 app.listen(port, () => {
