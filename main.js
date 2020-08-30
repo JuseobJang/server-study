3c2ca593b4079542cdd731ed3387b00da4be3472
JuseobJang <wntjq68@gmail.com>
2020-08-30 16:12:04 +0900

    feat: Handle error
    -400 error When requesting a url that does not exist, an error is processed immediately
    -500 error Requests a file that does not exist and handles an error when the content does not exist.

--- main.js
+++ main.js
@@ -36,11 +36,11 @@ app.get('/', (request, response) => {
   response.send(HTML); // html response
 })
 
-app.get('/page/:pageId', (request, response) => {
+app.get('/page/:pageId', (request, response, next) => {
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`./data/${filteredId}`, "utf-8", (err, description) => { //디렉토리 안의 id 와 이름이 같은 파일의 내용을 description으로 넣음
     if (err) {
-      throw err;
+      next(err);
     }
     var title = request.params.pageId;
 
@@ -139,7 +139,14 @@ app.post('/delete_process', (request, response) => {
   })
 })
 
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!"); // error 처리
+})
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
